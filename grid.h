d320d95031ac5be5a8523f39b083ffa9814245ea
Sam <sam.sutcliffe2@gmail.com>
2019-10-12 14:53:09 +0100

    Fixed intergration and rendering

--- grid.h
+++ grid.h
@@ -1,6 +1,7 @@
 #include "glm/glm.hpp"
 struct Grid{
     float Mass;
-    glm::vec2 Velocity;
-    glm::vec2 Force;
+    glm::vec2 Velocity = glm::vec2(0);
+    glm::vec2 Force = glm::vec2(0);
+    glm::vec2 Acceleration = glm::vec2(0);
 };
--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 #include "grid.h"
 #include "gif.h"
 #include <array>
+#include <bitset>
 #include <vector>
 constexpr static int MaxParticles = 1000;
 constexpr static int GridSize = 100;
-constexpr static float DeltaTime = 1e-2;
+constexpr static float DeltaTime = 1e-3;
+static constexpr int SubSteps = 100;
+static constexpr int Resolution = 10;
+static constexpr int RenderSize = GridSize * 10;
 int ParticleCount = 0;
 std::array<Particle,MaxParticles> ParticleList;
 std::array<Grid,GridSize * GridSize> SimGrid;
+std::vector<uint8_t> frame(RenderSize * RenderSize * 4, 0);
 Grid & GetGrid(int x, int y){
     return SimGrid[(x * GridSize) + y];
 }
@@ -29,9 +34,6 @@ glm::vec2 WeightGrad(glm::vec2 distance)
 {
     return glm::vec2(WeightGradAxis(distance.x) * WeightAxis(distance.y),
     WeightGradAxis(distance.y) * WeightAxis(distance.x));
-}
-void SaveParticles(){
-
 }
 void APIC(int x,int y,Particle p){
     Grid & g = GetGrid(x,y);
@@ -62,18 +64,21 @@ void P2G()
         if(g.Mass != 0)
         {
             g.Velocity /= g.Mass;
+            g.Acceleration = g.Force / g.Mass;
         }
     }
     for(int x = 0; x < GridSize;++x){
         GetGrid(x,1).Velocity.y = 0;
+        GetGrid(x,0).Velocity.y = 0;
     }
 }
-void G2PNode(int x,int y,Particle p){
-    Grid & g = GetGrid(x,y);
+void G2PNode(int x,int y,Particle & p){
+    Grid g = GetGrid(x,y);
     auto d = p.Position - glm::vec2(static_cast<float>(x),static_cast<float>(y));
     float weight = Weight(d);
     auto weightgrad = WeightGrad(d);
     p.Velocity += g.Velocity * weight;
+    p.Acceleration += g.Acceleration * weight;
     p.StrainRate[0][0] += g.Velocity.x * weightgrad.x;
     p.StrainRate[1][1] += g.Velocity.y * weightgrad.y;
     p.StrainRate[1][0] += (g.Velocity.x * weightgrad.y) + (g.Velocity.y * weightgrad.x);
@@ -83,16 +88,23 @@ void G2P()
 {
     for(int i = 0; i < ParticleCount;++i)
     {
-        auto particle = ParticleList[i];
+        auto & particle = ParticleList[i];
         int GridX = std::floor(particle.Position.x);
         int GridY = std::floor(particle.Position.y);
-        particle.Velocity = glm::vec2();
-        particle.StrainRate = glm::mat2x2();
+        particle.Velocity = glm::vec2(0);
+        particle.Acceleration = glm::vec2(0);
+        particle.StrainRate = glm::mat2x2(0);
         for(int x = 0;x < 2;++x)
         {
             for(int y = 0;y < 2;++y)
             {
-                G2PNode(GridX+x,GridY+y,particle);
+                if(GridX+x >=0 && GridX+x < GridSize)
+                {
+                    if(GridY+y >=0 && GridY+y < GridSize)
+                    {
+                        G2PNode(GridX+x,GridY+y,particle);
+                    }
+                }
             }
         }
     }
@@ -101,7 +113,9 @@ void Intergrate()
 {
     for(int i = 0; i < ParticleCount;++i)
     {
-        auto particle = ParticleList[i];
+        auto & particle = ParticleList[i];
+//        std::cout << "i:p:"<<particle.Position.x<<" " << particle.Position.y<<std::endl;
+//        std::cout << "i:v:"<<particle.Velocity.x<<" " << particle.Velocity.y<<std::endl;
         particle.Position += particle.Velocity * DeltaTime;
         particle.Velocity += particle.Acceleration * DeltaTime;
         particle.Strain += particle.StrainRate * DeltaTime;
@@ -122,38 +136,69 @@ void AddParticle(glm::vec2 pos){
     p.Position = pos;
     ParticleCount += 1;
 }
-int main(int argc, char ** args)
-{
-	std::vector<uint8_t> frame(GridSize * GridSize * 10 * 10 * 4, 0);
-	auto fileName = "out.gif";
-	int delay = 100;
-    for(int v = 0;v < 10;++v){
-        AddParticle(glm::vec2(30+(v/2.0),40));
-    }
-    int MaxTime = 100; 
-    GifWriter g;
-	GifBegin(&g, fileName, GridSize * 10, GridSize * 10, delay);
-    for(int t = 0;t < MaxTime;++t)
-    {
-        std::cout<<"T:"<<t<<std::endl;
-        //Update();
+void SaveParticles(GifWriter & g,int delay){
+
         for(int i = 0;i < ParticleCount;++i){
             auto p = ParticleList[i];
             for(int dx = 0;dx < 10;++dx){
                 for(int dy = 0;dy < 10;++dy){
-                    int x = p.Position.x + dx;
-                    int y = p.Position.y + dy;
-                    int v = 4 * ((x * 10) + (GridSize * 10 * y));
-                    if(x <= 0 && x <GridSize){
-                        if(y <= 0 && y <GridSize){
+                    int x = static_cast<int>((p.Position.x) * Resolution) + dx;
+                    int y = static_cast<int>((GridSize - p.Position.y) * Resolution) + dy;
+                    int v = 4 * (x + (RenderSize * y));
+                    if(x >= 0 && x < RenderSize){
+                        if(y >= 0 && y < RenderSize){
                             frame[v] = 255;
                         }
                     }
                 }
             }
         }
+}
+void PaintNumbers(int MaxTime,int t)
+{
+        float FillPC = (float)t / (float)MaxTime;
+        static constexpr int Bits = 5;
+        int fillint = static_cast<int>(FillPC * (float)(1<<(Bits-1)));
+        std::bitset<Bits> pattern = std::bitset<Bits>(fillint);
+        for(int b = 0;b < Bits;++b){
+            for(int dx = 0;dx < 10;++dx){
+                for(int dy = 0;dy < 10;++dy){
+                    int x = static_cast<int>(b * Resolution) + dx;
+                    int y = static_cast<int>(0) + dy;
+                    int v = 4 * (x + (RenderSize * y));
+                    if(x >= 0 && x < RenderSize){
+                        if(y >= 0 && y < RenderSize){
+                            frame[v+1] = 255;
+                        }
+                    }
+                }
+            }
+        }
+}
+int main(int argc, char ** args)
+{
+
+	auto fileName = "out.gif";
+	int delay = 10;
+    for(int v = 0;v < 10;++v){
+        AddParticle(glm::vec2(30.0+(v/2.0),40));
+    }
+    int MaxTime = 100; 
+    GifWriter g;
+	GifBegin(&g, fileName, RenderSize, RenderSize, delay);
+    for(int t = 0;t < MaxTime;++t)
+    {
+        std::cout<<"T:"<<t<<std::endl;
+        for(int i = 0; i< SubSteps;++i){
+            Update();
+        }
+        std::fill(frame.begin(),frame.end(),0);
+        SaveParticles(g,delay);
+        PaintNumbers(MaxTime,t);
         GifWriteFrame(&g, frame.data(), GridSize * 10, GridSize * 10, delay);
     }
+    std::fill(frame.begin(),frame.end(),255);
+    GifWriteFrame(&g, frame.data(), GridSize * 10, GridSize * 10, delay);
 	GifEnd(&g);
     std::cout<<"Finished"<<std::endl;
 }
