6285ebfc3db4416f99d44c06e9248709c3a6d4fd
Sam <sam.sutcliffe2@gmail.com>
2021-07-04 15:10:38 +0100

    updated build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ *.cpp -O3 -ffast-math -std=c++2a -fopenmp
+g++ *.cpp -O3 -ffast-math -std=c++2a -fopenmp && ./a.out && ffmpeg -framerate 60  -i './Frames/frame %d .png' -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4 -y
